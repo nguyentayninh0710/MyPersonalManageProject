78c7c1c01066c64968f312a8b70bae8fd30faa3c
nguyentayninh0710 <huynhkykhoinguyen0710@gmail.com>
2023-02-01 13:45:25 +0700

    first commit

---
+++ src/main/java/com/myclass/main/MyPersonalManageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myclass.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalManageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalManageProjectApplication.class, args);
+	}
+
+}
